6948cdadbfb98264a82a09aa94dc4f38edf08e72
David Radchenko <davidradchenko2@gmail.com>
2023-06-20 08:35:55 -0700

    end 1440px

--- js/index.js
+++ js/index.js
@@ -1,62 +1,81 @@
 const buttons = document.querySelectorAll('.accordion-button');
 const mainImage = document.getElementById('main-image');
+const block1 = document.getElementById('Wallmer-wind-1');
+const block2 = document.getElementById('Wallmer-wind-2');
+const block3 = document.getElementById('Wallmer-wind-3');
+const block4 = document.getElementById('Wallmer-wind-4');
+const block5 = document.getElementById('Wallmer-wind-5');
 
 buttons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        const item = button.parentNode;
-        const isActive = item.classList.contains('active');
+  button.addEventListener('click', () => {
+    const item = button.parentNode;
+    const isActive = item.classList.contains('active');
 
-        if (!isActive) {
-            closeAllAccordions();
-            item.classList.add('active');
-            button.querySelector('.accordion-icon').textContent = '-';
-            updateMainImage(index);
-        } else {
-        }
-    });
+    if (!isActive) {
+      closeAllAccordions();
+      item.classList.add('active');
+      button.querySelector('.accordion-icon').src = "./img/Wallmer/minus.svg";
+      updateMainImage(index);
+    } else {}
+  });
 });
 
 function closeAllAccordions() {
-    const items = document.querySelectorAll('.item');
-    items.forEach(item => {
-        item.classList.remove('active');
-        item.querySelector('.accordion-icon').textContent = '+';
-    });
+  const items = document.querySelectorAll('.item');
+  items.forEach(item => {
+    item.classList.remove('active');
+    item.querySelector('.accordion-icon').src = "./img/Wallmer/add.svg";
+  });
 }
 
 function updateMainImage(index) {
-    switch (index) {
-        case 0:
-            mainImage.src = './img/Wallmer/Photos-Accordion/window.png';
-            mainImage.alt = 'Изображение 1';
-            break;
-        case 1:
-            mainImage.src = './img/Wallmer/Photos-Accordion/profileImage.png';
-            mainImage.alt = 'Изображение 2';
-            break;
-        case 2:
-            mainImage.src = './img/Wallmer/Photos-Accordion/window.png';
-            mainImage.alt = 'Изображение 3';
-            break;
-        case 3:
-            mainImage.src = './img/Wallmer/Photos-Accordion/profileImage.png';
-            mainImage.alt = 'Изображение 4';
-            break;
-        case 4:
-                mainImage.src = './img/Wallmer/Photos-Accordion/window.png';
-                mainImage.alt = 'Изображение 4';
-                break;
-        default:
-            break;
-    }
+  switch (index) {
+    case 0:
+      block1.classList.add('classBlock')
+      block2.classList.remove('classBlock')
+      block3.classList.remove('classBlock')
+      block4.classList.remove('classBlock')
+      block5.classList.remove('classBlock')
+      break;
+    case 1:
+      block1.classList.remove('classBlock')
+      block2.classList.add('classBlock')
+      block3.classList.remove('classBlock')
+      block4.classList.remove('classBlock')
+      block5.classList.remove('classBlock')
+      break;
+    case 2:
+      block1.classList.remove('classBlock')
+      block2.classList.remove('classBlock')
+      block3.classList.add('classBlock')
+      block4.classList.remove('classBlock')
+      block5.classList.remove('classBlock')
+      break;
+    case 3:
+      block1.classList.remove('classBlock')
+      block2.classList.remove('classBlock')
+      block3.classList.remove('classBlock')
+      block4.classList.add('classBlock')
+      block5.classList.remove('classBlock')
+      break;
+    case 4:
+      block1.classList.remove('classBlock')
+      block2.classList.remove('classBlock')
+      block3.classList.remove('classBlock')
+      block4.classList.remove('classBlock')
+      block5.classList.add('classBlock')
+      break;
+    default:
+      break;
+  }
 }
-function resetMainImage() {
 
-    mainImage.src = './img/Wallmer/Photos-Accordion/window.png'; 
-    mainImage.alt = 'Изображение'; 
+function resetMainImage() {
+  mainImage.src = './img/Wallmer/Photos-Accordion/window.png';
+  mainImage.alt = 'Изображение';
 }
 document.querySelector('.item:first-child').classList.add('active');
-document.querySelector('.item:first-child .accordion-icon').textContent = '-';
+document.querySelector('.item:first-child .accordion-icon').src = "./img/Wallmer/minus.svg";
 updateMainImage(0);
 
 
@@ -96,13 +115,13 @@ function updateSliderPosition() {
 
   sliderItems.forEach((item, index) => {
     item.classList.remove('centered', 'right-aligned');
-    
+
     if (index === currentPosition) {
-     
+
     } else if (index === currentPosition + 1) {
       item.classList.add('centered');
-    }else if(index === currentPosition + 2){
-        item.classList.add('right-aligned');
+    } else if (index === currentPosition + 2) {
+      item.classList.add('right-aligned');
     }
   });
 
